c94d3684b0a8b6a4a3bd5663f8c76fda3739170e
siam383425-lgtm <siam383425@gmail.com>
2026-03-25 00:45:31 +0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'fun-corner-v1';
+const urlsToCache = [
+  '/',
+  'index.html',
+  'manifest.json',
+  'logo.jpeg',
+  'owner.jpg',
+  'https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;500;600;700;800;900&display=swap',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
